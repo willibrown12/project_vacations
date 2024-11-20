97ec6c77c929c6a11701ffaecc9d8d527fadc9ed
willibrown12 <viliking123@gmail.com>
2024-11-20 10:03:11 +0200

    docker

--- api-client/index.js
+++ api-client/index.js
@@ -1,7 +1,16 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('public'))
+// Serve static files from the 'public' directory (built React app)
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(4200)
+// Serve the React app for any other routes (this ensures routing works)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.listen(4200, () => {
+  console.log('Server is running on port 4200');
+});
